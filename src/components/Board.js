21815ad21a1e2464665b1e12356cab22bafc7eaa
seo-woo <87346418+seo-woo@users.noreply.github.com>
2023-07-17 22:47:06 +0900

    update

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,50 +1,84 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 import "./Board.css";
 
-export default class Board extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
+const Board = () => {
+
+  const[squares, setSquares] = useState(Array(9).fill(null));
+  const[xIsNext, setXIsNext] = useState(true);
+
+  const calculaterWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let index = 0; index < lines.length; index++) {
+      const [a,b,c] = lines[index];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }    
+    }
+    return null;
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+  const winner = calculaterWinner(squares); 
+  let status;
+  if(winner){
+    status = 'Winner:' + winner;
+  }else{
+    status = `Next player: ${xIsNext? "X" : "O"}`;
   }
 
-  renderSquare(i) {
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+    if(calculaterWinner(newSquares) || newSquares[i]){
+        return;
+    }
+
+    newSquares[i] = xIsNext ? "X" : "O";
+    setSquares(newSquares);
+    setXIsNext(prev => !prev)
+  }
+
+
+
+  const renderSquare = (i) => {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={squares[i]}
+        onClick={() => handleClick(i)}
       />
     );
   }
 
-  render() {
-    return (
-      <div>
-        <div className="status">Next Player: X, O</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+
 }
+
+export default Board;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Square.css";
 
-export default class Square extends React.Component {
-  render() {
+const Square = ({onClick, value}) => {
+  
     return (
       <button
         className="square"
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
+        onClick={onClick}>
+        {value}
       </button>
     );
-  }
+
 }
+
+
+export default Square;
